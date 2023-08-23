6f28a678804dbe54033fe76d0b5b9797dbc6ebdc
unknown <qrdej31@gmail.com>
2023-08-23 20:04:38 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <div className='bg-pink-200'>
+      <Button>HELLO WORLD</Button>
+    </div>
+  );
+}
+
+export default App;
